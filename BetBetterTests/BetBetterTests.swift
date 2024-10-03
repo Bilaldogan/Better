e9fdebc7cca4d435b3de0db64b3bada0e3c8d8cb
Bilal <bilal@Aleyna-MacBook-Pro.local>
2024-10-03 16:27:39 +0300

    initial commit

---
+++ BetBetterTests/BetBetterTests.swift
@@ -0,0 +1,17 @@
+//
+//  BetBetterTests.swift
+//  BetBetterTests
+//
+//  Created by Bilal on 3.10.2024.
+//
+
+import Testing
+@testable import BetBetter
+
+struct BetBetterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
